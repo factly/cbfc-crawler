96f6c3beed9cee89655a0a6f5783395d23f67a5f
Monark <monark@factly.in>
2020-03-31 14:40:13 +0530

    bug solved

--- cbfc/spiders/movies_patch.py
+++ cbfc/spiders/movies_patch.py
@@ -40,42 +40,53 @@ class MoviesPatchSpider(scrapy.Spider):
             )
 
     def parse_details_page(self, response):
+        print(response.meta['mid'])
         details = response.css("table")[0].css("tr")
-        m_name = details[1].css("td")[1].css("::text").extract_first()
-        m_lang = details[2].css("td")[1].css("::text").extract_first()
-        m_category = details[3].css("td")[1].css("::text").extract_first()
-        c_office = details[4].css("td")[1].css("::text").extract_first()
-        c_no = details[5].css("td")[1].css("::text").extract_first()
-        c_date = details[6].css("td")[1].css("::text").extract_first()
-        m_leng = details[7].css("td")[1].css("::text").extract_first()
-        m_producer = details[8].css("td")[1].css("::text").extract_first()
-        c_applicant = details[9].css("td")[1].css("::text").extract_first()
 
-        cutsList = list()
-        
-        if len(response.css("table")) == 2:
-            cuts = response.css("table")[1].css("tr")    
-            if len(cuts) > 2:
-                for cut in cuts[2:]:
-                    each = dict()
-                    each['where'] = cut.css("td")[1].css("::text").extract_first()
-                    each['desc'] = cut.css("td")[2].css("::text").extract_first()
-                    each['guidline'] = cut.css("td")[3].css("::text").extract_first()
-                    cutsList.append(each)
+        if len(details) == 2:
+            error_message = {
+                "m_id" : response.meta['mid'],
+                "l_id": response.meta['lid'],
+                "error": "DATA_NOT_FOUND"
+            }
+            self.error.write(json.dumps(error_message) + "\n")
+
+        else:
+            m_name = details[1].css("td")[1].css("::text").extract_first()
+            m_lang = details[2].css("td")[1].css("::text").extract_first()
+            m_category = details[3].css("td")[1].css("::text").extract_first()
+            c_office = details[4].css("td")[1].css("::text").extract_first()
+            c_no = details[5].css("td")[1].css("::text").extract_first()
+            c_date = details[6].css("td")[1].css("::text").extract_first()
+            m_leng = details[7].css("td")[1].css("::text").extract_first()
+            m_producer = details[8].css("td")[1].css("::text").extract_first()
+            c_applicant = details[9].css("td")[1].css("::text").extract_first()
+
+            cutsList = list()
             
-        yield Movie (
-            m_id = response.meta['mid'],
-            movie_name = m_name,
-            movie_language = m_lang,
-            movie_category = m_category,
-            certificate_reg_office = c_office,
-            certificate_no = c_no,
-            certificate_date = c_date,
-            movie_length = m_leng,
-            movie_producer = m_producer,
-            certificate_applicant = c_applicant,
-            cuts = cutsList
-        )
+            if len(response.css("table")) == 2:
+                cuts = response.css("table")[1].css("tr")    
+                if len(cuts) > 2:
+                    for cut in cuts[2:]:
+                        each = dict()
+                        each['where'] = cut.css("td")[1].css("::text").extract_first()
+                        each['desc'] = cut.css("td")[2].css("::text").extract_first()
+                        each['guidline'] = cut.css("td")[3].css("::text").extract_first()
+                        cutsList.append(each)
+                
+            yield Movie (
+                m_id = response.meta['mid'],
+                movie_name = m_name,
+                movie_language = m_lang,
+                movie_category = m_category,
+                certificate_reg_office = c_office,
+                certificate_no = c_no,
+                certificate_date = c_date,
+                movie_length = m_leng,
+                movie_producer = m_producer,
+                certificate_applicant = c_applicant,
+                cuts = cutsList
+            )
 
     def error_handler(self,failure):
         error_message = {
